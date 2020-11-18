568169d645df97bb4296b225bdb2beecf5f0642d
XuanThai <mknumberone9x@gmail.com>
2020-11-18 23:33:53 +0700

    5.Intro styled components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import AboutUS from './pages/AboutUS';
+//Global style
+import GlobalStyle from './components/GlobalStyle'
 //Import Page
 import AboutUs from './pages/AboutUS'
 class App extends Component {
   render() {
     return (
       <div>
-        <AboutUS></AboutUS>
+        <GlobalStyle/>
+        <AboutUS/>
       </div>
     );
   }
