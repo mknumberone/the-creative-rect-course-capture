85ac3467666bff6e81e16c095010576cbb34b776
XuanThai <mknumberone9x@gmail.com>
2020-11-18 22:52:43 +0700

    3.FaqSection

---
+++ src/components/FaqSection.js
@@ -0,0 +1,59 @@
+import React from 'react';
+
+const FaqSection = () => {
+    return (
+        <div className="faq">
+            <h2>Any Question <span>FAQ</span></h2>
+            <div className="question">
+                <h4>How Do I Start?</h4>
+                <div className="answer">
+                    <p>Lorem Ipsum is simply dummy text.</p>
+                    <p>
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                        Lorem Ipsum has been the industry's standard dummy text ever since the
+                        1500s, when an unknown printer took a galley of type and scrambled it to
+                        make a type specimen book.
+                    </p>
+                </div>
+            </div>
+            <div className="question">
+                <h4>Daily Schedula</h4>
+                <div className="answer">
+                    <p>Lorem Ipsum is simply dummy text.</p>
+                    <p>
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                        Lorem Ipsum has been the industry's standard dummy text ever since the
+                        1500s, when an unknown printer took a galley of type and scrambled it to
+                        make a type specimen book.
+                    </p>
+                </div>
+            </div>
+            <div className="question">
+                <h4>Differrent Payment Method</h4>
+                <div className="answer">
+                    <p>Lorem Ipsum is simply dummy text.</p>
+                    <p>
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                        Lorem Ipsum has been the industry's standard dummy text ever since the
+                        1500s, when an unknown printer took a galley of type and scrambled it to
+                        make a type specimen book.
+                    </p>
+                </div>
+            </div>
+            <div className="question">
+                <h4>What product do you Offer?</h4>
+                <div className="answer">
+                    <p>Lorem Ipsum is simply dummy text.</p>
+                    <p>
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                        Lorem Ipsum has been the industry's standard dummy text ever since the
+                        1500s, when an unknown printer took a galley of type and scrambled it to
+                        make a type specimen book.
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default FaqSection
--- src/pages/AboutUS.js
+++ src/pages/AboutUS.js
@@ -2,11 +2,13 @@ import React from 'react';
 //Page component
 import AboutSection from '../components/AboutSection'
 import SevicesSection from '../components/ServicesSection'
+import FaqSection from '../components/FaqSection'
 const AboutUS = () => {
     return (
         <>
             <AboutSection />
             <SevicesSection />
+            <FaqSection/>
         </>
     )
 }
