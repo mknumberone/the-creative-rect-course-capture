15d4a3c42ee3207fcbb9a4a63e8baafeb3b64ee7
XuanThai <mknumberone9x@gmail.com>
2020-11-19 14:48:07 +0700

    7.Style Faq

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,8 +1,9 @@
 import React from 'react';
-
+import styled from 'styled-components'
+import {About,Description,Hide,Image} from '../styles'
 const FaqSection = () => {
     return (
-        <div className="faq">
+      <Faq>
             <h2>Any Question <span>FAQ</span></h2>
             <div className="question">
                 <h4>How Do I Start?</h4>
@@ -15,6 +16,7 @@ const FaqSection = () => {
                         make a type specimen book.
                     </p>
                 </div>
+                <div className="faq-line"></div>
             </div>
             <div className="question">
                 <h4>Daily Schedula</h4>
@@ -27,7 +29,9 @@ const FaqSection = () => {
                         make a type specimen book.
                     </p>
                 </div>
+                <div className="faq-line"></div>
             </div>
+            
             <div className="question">
                 <h4>Differrent Payment Method</h4>
                 <div className="answer">
@@ -39,6 +43,7 @@ const FaqSection = () => {
                         make a type specimen book.
                     </p>
                 </div>
+                <div className="faq-line"></div>
             </div>
             <div className="question">
                 <h4>What product do you Offer?</h4>
@@ -51,9 +56,35 @@ const FaqSection = () => {
                         make a type specimen book.
                     </p>
                 </div>
+                <div className="faq-line"></div>
             </div>
-        </div>
+            </Faq>
     )
 }
-
+const Faq = styled(About)`
+display: block;
+span{
+    display: block;
+}
+h2{
+    padding-bottom:2rem;
+    font-weight:lighter;
+}
+.faq-line{
+    background: #cccccc;
+    height:0.2rem;
+    margin:2rem 0rem;
+    width:100% ;
+}
+.question{
+    padding:3rem 0rem;
+    cursor: pointer;
+}
+.awser{
+    padding:2rem 0rem;
+    p{
+        padding:1rem 0rem;
+    }
+}
+`
 export default FaqSection
